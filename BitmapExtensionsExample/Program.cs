f15f54db3e3fd2dd9980ad1b25c2829c36fa7d9c
wyckster <wyckster@hotmail.com>
2020-12-09 12:53:05 -0500

    0.1.0

---
+++ BitmapExtensionsExample/Program.cs
@@ -0,0 +1,58 @@
+﻿using BitmapExtensionsLib;
+using RGBLib;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitmapExtensionsExample
+{
+    class Program
+    {
+        static double Checker(int x, int y)
+        {
+            bool c = ((x ^ y) & 1) != 0;
+            return c ? 1.0 : 0.0;
+        }
+
+        static void Main(string[] args)
+        {
+            // Example 1:
+            //   1) Create a 4x4 orange bitmap with a blue square in the middle.
+            //   2) Convert it to a 2-dimensional array of linear RGB values.
+            //   3) Examine some of the pixels' linear RGB values.
+            Bitmap bitmap = new Bitmap(4, 4);
+            Graphics g = Graphics.FromImage(bitmap);
+            g.Clear(Color.FromArgb(255, 128, 0)); // orange
+            g.FillRectangle(new SolidBrush(Color.FromArgb(0, 0, 255)), 1, 1, 2, 2);
+            RGB[,] rgb = bitmap.ToRGBArray();
+            Console.WriteLine("Should be orange.  The values are linear, so expect full red, green value of about 0.215 and no blue:\n\t{0}", rgb[0, 0]);
+            Console.WriteLine("Should be blue:\n\t{0}", rgb[1, 1]);
+
+
+            // Example 2:  Render, where pixel colour is a function of x and y.
+            //   1) Create a 256x256 image.
+            //   2) Fill the image with colours.
+            //   3) Save the file.
+            //
+            // The image consists of:
+            //   - A linear gradient on the top half,
+            //   - An SRGB gradient on the bottom half,
+            //   - A strip between them with 50% black and white dot pattern.
+            Bitmap ramp = new Bitmap(256, 256);
+            ramp.Render(delegate (int x, int y) {
+                if (y < 128 - 16) {
+                    return new RGB(x / 255.0, x / 255.0, x / 255.0);
+                } else if (y >= (128 + 16)) {
+                    return new SRGBA((byte)x, (byte)x, (byte)x).ToRGB();
+                } else {
+                    return new RGB(Checker(x, y), Checker(x, y), Checker(x, y));
+                }
+            });
+            ramp.Save("linear-vs-nonlinear.png", ImageFormat.Png);
+        }
+    }
+}
--- BitmapExtensionsExample/Properties/AssemblyInfo.cs
+++ BitmapExtensionsExample/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BitmapExtensionsLib")]
+[assembly: AssemblyTitle("BitmapExtensionsExample")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("Bitmap Extensions")]
-[assembly: AssemblyCompany("Chad Faragher")]
-[assembly: AssemblyProduct("BitmapExtensionsLib")]
-[assembly: AssemblyCopyright("Copyright © 2020 Chad Faragher")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BitmapExtensionsExample")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4f237ac5-f182-4761-98d1-499a48142cc5")]
+[assembly: Guid("0bbf5eaf-9e4b-491f-81aa-f3148b9756ee")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
